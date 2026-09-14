15cf0fa417a973d0ad649ba709c0f396672aef81
Victor <capitellivictor@gmail.com>
2026-09-14 18:55:13 -0300

    feature: Receivable Mapper (MapStruct);

---
+++ back-end/src/main/java/com/srm/credit_engine/mapper/ReceivableMapper.java
@@ -0,0 +1,20 @@
+package com.srm.credit_engine.mapper;
+
+import com.srm.credit_engine.controller.dto.CreateReceivableRequest;
+import com.srm.credit_engine.controller.dto.ReceivableResponse;
+import com.srm.credit_engine.domain.entity.Receivable;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ReceivableMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "settlement", ignore = true)
+    Receivable toEntity(CreateReceivableRequest request);
+
+    ReceivableResponse toResponse(Receivable receivable);
+}
