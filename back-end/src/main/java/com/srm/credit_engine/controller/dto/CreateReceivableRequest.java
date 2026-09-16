b03214200990693905109ce0991306b969872ac2
Victor <capitellivictor@gmail.com>
2026-09-16 15:08:58 -0300

    fix: Receivable (request and response) and Settlement response DTO's now has Assignor;

--- back-end/src/main/java/com/srm/credit_engine/controller/dto/CreateReceivableRequest.java
+++ back-end/src/main/java/com/srm/credit_engine/controller/dto/CreateReceivableRequest.java
@@ -7,12 +7,18 @@ import com.srm.credit_engine.domain.enums.CurrencyCode;
 import com.srm.credit_engine.domain.enums.ReceivableType;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateReceivableRequest(
         @NotNull
         @DecimalMin(value = "0.0", inclusive = false)
         BigDecimal faceValue,
+
+        @NotBlank
+        @Size(min = 11, max = 14)
+        String assignor,
 
         @NotNull
         ReceivableType type,
--- back-end/src/main/java/com/srm/credit_engine/controller/dto/SettlementResponse.java
+++ back-end/src/main/java/com/srm/credit_engine/controller/dto/SettlementResponse.java
@@ -7,6 +7,7 @@ import com.srm.credit_engine.domain.enums.CurrencyCode;
 
 public record SettlementResponse(
         Long receivableId,
+        String assignor,
         BigDecimal presentValue,
         BigDecimal discount,
         CurrencyCode paymentCurrency,
